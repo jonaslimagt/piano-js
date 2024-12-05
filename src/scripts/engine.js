f0bcecd2bcb685d8b6400244abdb7dc61742f714
Jonas Lima <jonaslimagt@gmail.com>
2024-12-05 15:38:29 -0300

    Commit 006: make the header buttons functional

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,18 +1,38 @@
-const pianoKeys = document.querySelectorAll(".piano-keys .key");
+const pianoKeys = document.querySelectorAll(".piano-keys .key"), 
+volumeSlider = document.querySelector(".volume-slider input"),
+keyCheckbox = document.querySelector(".key-check input");
 
-let audio = new Audio("src/audio/a.wav");
+let allKeys = [], audio = new Audio("src/audio/a.wav");
 
 const playTune = (key) => {
     audio.src = `src/audio/${key}.wav`;
     audio.play();
+
+    const clickedKey = document.querySelector(`[data-key="${key}"]`);
+    clickedKey.classList.add("active");
+    setTimeout(() => {
+        clickedKey.classList.remove("active");
+    }, 150);
 };
 
 pianoKeys.forEach((key) => {
+    allKeys.push(key.dataset.key);
+
     key.addEventListener("click", () => playTune(key.dataset.key));
 });
 
-document.addEventListener("keydown", (event) => {
-    try {
-        playTune(event.key);
-    } catch (error) {}
-});
+const handleVolume = (event) => {
+    audio.volume = event.target.value;
+}
+
+const showHideKeys = (event) => {
+    pianoKeys.forEach(key => key.classList.toggle("hide"));
+}
+
+const pressedKey = (event) => {
+    if (allKeys.includes(event.key)) playTune(event.key);
+}
+
+volumeSlider.addEventListener("input", handleVolume);
+keyCheckbox.addEventListener("click", showHideKeys);
+document.addEventListener("keydown", pressedKey);
